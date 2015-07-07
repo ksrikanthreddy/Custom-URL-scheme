6d5fb3c29529f144281d76b4e210ffdb76936d6f
Srikanth Reddy <kollisreekanth@gmail.com>
2015-07-07 09:27:43 +0530

    Update LaunchMyApp.java

--- src/android/nl/xservices/plugins/LaunchMyApp.java
+++ src/android/nl/xservices/plugins/LaunchMyApp.java
@@ -13,24 +13,35 @@ import java.io.StringWriter;
 import java.io.Writer;
 import java.util.Locale;
 
+import android.util.Log;
+
 public class LaunchMyApp extends CordovaPlugin {
+
 	private static final String TAG = "LaunchMyApp";
+	
   private static final String ACTION_CHECKINTENT = "checkIntent";
 
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		Log.e(TAG, "action=" + action);
     if (ACTION_CHECKINTENT.equalsIgnoreCase(action)) {
+			Log.e(TAG, "if1=true");
       final Intent intent = ((CordovaActivity) this.webView.getContext()).getIntent();
       final String intentString = intent.getDataString();
-      if (intentString != null && intentString.contains("://")) {
+			Log.e(TAG, "intentString=" + intentString);
+      if (intentString != null && intentString.contains("://") &&
+          intent.getScheme() != null && intent.hasCategory(Intent.CATEGORY_BROWSABLE)) {
         callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, intent.getDataString()));
         intent.setData(null);
+				Log.e(TAG, "if2=true");
         return true;
       } else {
         callbackContext.error("App was not started via the launchmyapp URL scheme. Ignoring this errorcallback is the best approach.");
+				Log.e(TAG, "if2=false");
         return false;
       }
     } else {
+			Log.e(TAG, "if1=false");
       callbackContext.error("This plugin only responds to the " + ACTION_CHECKINTENT + " action.");
       return false;
     }
